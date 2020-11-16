dacb78802ed6fcf1ca5859b7a0fecd1216611a35
UsaamahG <71952590+UG@users.noreply.github.com>
2020-11-16 16:40:07 -0500

    Updating products.js for array, added image sources, updated descriptions and rating.

--- js/products.js
+++ js/products.js
@@ -1,142 +1,82 @@
 // array of product objects
 var products = [{
         id: 1,
-        title: "Introductory Intergalactic Mech",
-        price: 599,
-        description: "Mech for war",
-        imgsrc: "img/element2.png",
-        rate: "5"
-    },
-    {
-        id: 2,
-        title: "Ship",
-        price: 900,
-        description: "shp for space",
-        imgsrc: "img/element2.png",
-        rate: "4"
-    },
-    {
-        id: 3,
-        title: "toilet",
-        price: 1200,
-        description: "pooping in out of space :)",
-        imgsrc: "img/element2.png",
-        rate: "1"
-    },
-    {
-        id: 4,
-        title: "MK890",
-        price: 200,
-        description: "Ray gun",
-        imgsrc: "img/element2.png",
+        title: "Dominus Battle Car",
+        price: 199,
+        description: "Popular across the Galaxy, this is a must have staple!",
+        imgsrc: "img/car1.png",
         rate: "2"
     },
     {
+        id: 2,
+        title: "Komodo Battle Car",
+        price: 400,
+        description: "An aggressive sports model for all the tuners out there",
+        imgsrc: "img/car2.png",
+        rate: "2.5"
+    },
+    {
+        id: 3,
+        title: "Dark Knight Rises Tumbler",
+        price: 2500,
+        description: "For the heaviest duty battle in the Galaxy. Used by Bruce Wayne in Gotham",
+        imgsrc: "img/car3.png",
+        rate: "4.5"
+    },
+    {
+        id: 4,
+        title: "99' Nissan Skyline GTR R34",
+        price: 1500,
+        description: "A classic for the Japanese sports scene of the 90s! Now joining battle",
+        imgsrc: "img/car4.png",
+        rate: "5"
+    },
+    {
         id: 5,
-        title: "Von Braun thrusters",
-        price: 1200,
-        description: "rocket thrusters",
-        imgsrc: "img/element2.png",
+        title: "De'Lorean Time Machine",
+        price: 5000,
+        description: "Back to the Future!",
+        imgsrc: "img/car5.png",
         rate: "4"
     },
     {
         id: 6,
-        title: "Exoskeleton",
-        price: 1800,
-        description: "wearable exo adds 100lbs to your lift",
-        imgsrc: "img/element2.png",
-        rate: "5"
+        title: "Vulcan Battle Car",
+        price: 500,
+        description: "Battle Car made by our galactic neightbors to join the games. Now sold to Earth!",
+        imgsrc: "img/car6.png",
+        rate: "2"
     },
     {
         id: 7,
-        title: "jetpack",
-        price: 1700,
-        description: "air pressured jetpacks for zero g's",
-        imgsrc: "img/element2.png",
-        rate: "4"
+        title: "Animus GP Battle Car",
+        price: 999,
+        description: "Formula One racer enters the battle!",
+        imgsrc: "img/car7.png",
+        rate: "3"
     },
     {
         id: 8,
-        title: "Nuclear batteries",
-        price: 12200,
-        description: "Nuclear powered battery",
-        imgsrc: "img/element2.png",
-        rate: "4"
+        title: "Breakout Type-S",
+        price: 599,
+        description: "Re-invention of a classic Earth-era Rocket battle car",
+        imgsrc: "img/car8.png",
+        rate: "1.5"
     },
     {
         id: 9,
-        title: "Helmet",
-        price: 1200,
-        description: "Electronic helmet with A.I",
-        imgsrc: "img/element2.png",
-        rate: "4"
+        title: "Peregrine TT",
+        price: 1250,
+        description: "Designed by the Covenant to enter the games. Available for the first time!",
+        imgsrc: "img/car9.png",
+        rate: "0.5"
     },
     {
         id: 10,
-        title: "holoprojector",
-        price: 1200,
-        description: "projects your voice calls into holograms",
-        imgsrc: "img/element2.png",
-        rate: "4"
-    },
-    {
-        id: 11,
-        title: "solar panels ",
-        price: 100,
-        description: "solar panels can recharge nuclear batteries with solar winds ",
-        imgsrc: "img/element2.png",
-        rate: "4"
+        title: "Hotwheels Twin Mill III",
+        price: 750,
+        description: "HotWheels classic enters the battle!",
+        imgsrc: "img/car10.png",
+        rate: "1"
     }
 ];
-
-
-function loadProduct(index) {
-    // loads products with index chosen to p
-    var p = products[index];
-    // grabs the title from the title class html
-    var title = document.getElementsByClassName("product-title").item(2);
-    var price = document.getElementsByClassName("priceinfo").item(index);
-    var description = document.getElementsByClassName("product-description").item(index);
-    var rating = document.getElementsByClassName("rate").item(index);
-    var description = document.getElementsByClassName("product-description").item(index);
-    title.innerText = p.title;
-    price.innerText = p.price;
-    description.innerText = p.description;
-    rating.innerText = p.rate;
-}
-
-loadProduct(2)
-
-
-function loadProduct(index) {
-    // loads products with index chosen to p
-    var p = products[index]
-        // grabs the title from the title class html
-    var title = document.getElementsByClassName("product-title").item(0)
-    var price = document.getElementsByClassName("priceinfo").item(0)
-    title.innerText = p.title
-    price.innerText = p.price
-    var description = document.getElementsByClassName("product-description").item(0)
-    description.innerText = p.description
-
-
-
-
-    var img = document.getElementsByClassName("product-image").item(0)
-    img.setAttribute("src", p.imgsrc)
-
-    updatePrice(p)
-
-}
-
-function updatePrice(p) {
-    var quantity = document.getElementsByName("quantityinput")
-    var price = document.getElementsByClassName("priceinfo").item(0)
-    var totalPrice = p.quantity * p.price
-
-    price.innerText = p.price
-
-
-    console.log(totalPrice)
-
-}
