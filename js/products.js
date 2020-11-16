e36c870db68896cbc937146c35e79d3dacc2c740
Koji <48003543+0ji@users.noreply.github.com>
2020-11-16 18:41:31 -0500

    merged products

--- js/products.js
+++ js/products.js
@@ -86,5 +86,85 @@ var products = [{
         description: "solar panels can recharge nuclear batteries with solar winds ",
         imgsrc: "img/element2.png",
         rate: 4
+    },
+    {
+        id: 12,
+        title: "Dominus Battle Car",
+        price: 199,
+        description: "Popular across the Galaxy, this is a must have staple!",
+        imgsrc: "img/car1.png",
+        rate: 2
+    },
+    {
+        id: 13,
+        title: "Komodo Battle Car",
+        price: 400,
+        description: "An aggressive sports model for all the tuners out there",
+        imgsrc: "img/car2.png",
+        rate: 2.5
+    },
+    {
+        id: 14,
+        title: "Dark Knight Rises Tumbler",
+        price: 2500,
+        description: "For the heaviest duty battle in the Galaxy. Used by Bruce Wayne in Gotham",
+        imgsrc: "img/car3.png",
+        rate: 4.5
+    },
+    {
+        id: 15,
+        title: "99' Nissan Skyline GTR R34",
+        price: 1500,
+        description: "A classic for the Japanese sports scene of the 90s! Now joining battle",
+        imgsrc: "img/car4.png",
+        rate: 5
+    },
+    {
+        id: 16,
+        title: "De'Lorean Time Machine",
+        price: 5000,
+        description: "Back to the Future!",
+        imgsrc: "img/car5.png",
+        rate: 4
+    },
+    {
+        id: 17,
+        title: "Vulcan Battle Car",
+        price: 500,
+        description: "Battle Car made by our galactic neightbors to join the games. Now sold to Earth!",
+        imgsrc: "img/car6.png",
+        rate: 2
+    },
+    {
+        id: 18,
+        title: "Animus GP Battle Car",
+        price: 999,
+        description: "Formula One racer enters the battle!",
+        imgsrc: "img/car7.png",
+        rate: 3
+    },
+    {
+        id: 19,
+        title: "Breakout Type-S",
+        price: 599,
+        description: "Re-invention of a classic Earth-era Rocket battle car",
+        imgsrc: "img/car8.png",
+        rate: 1.5
+    },
+    {
+        id: 20,
+        title: "Peregrine TT",
+        price: 1250,
+        description: "Designed by the Covenant to enter the games. Available for the first time!",
+        imgsrc: "img/car9.png",
+        rate: 0.5
+    },
+    {
+        id: 21,
+        title: "Hotwheels Twin Mill III",
+        price: 750,
+        description: "HotWheels classic enters the battle!",
+        imgsrc: "img/car10.png",
+        rate: 1
     }
 ];
