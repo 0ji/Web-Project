2a5a9519f9ac1f59e69530de2602ef5fe7f1e093
UsaamahG <71952590+UG@users.noreply.github.com>
2020-11-16 18:04:49 -0500

    updated rate from string to numebr

--- js/products.js
+++ js/products.js
@@ -5,7 +5,7 @@ var products = [{
         price: 199,
         description: "Popular across the Galaxy, this is a must have staple!",
         imgsrc: "img/car1.png",
-        rate: "2"
+        rate: 2
     },
     {
         id: 2,
@@ -13,7 +13,7 @@ var products = [{
         price: 400,
         description: "An aggressive sports model for all the tuners out there",
         imgsrc: "img/car2.png",
-        rate: "2.5"
+        rate: 2.5
     },
     {
         id: 3,
@@ -21,7 +21,7 @@ var products = [{
         price: 2500,
         description: "For the heaviest duty battle in the Galaxy. Used by Bruce Wayne in Gotham",
         imgsrc: "img/car3.png",
-        rate: "4.5"
+        rate: 4.5
     },
     {
         id: 4,
@@ -29,7 +29,7 @@ var products = [{
         price: 1500,
         description: "A classic for the Japanese sports scene of the 90s! Now joining battle",
         imgsrc: "img/car4.png",
-        rate: "5"
+        rate: 5
     },
     {
         id: 5,
@@ -37,7 +37,7 @@ var products = [{
         price: 5000,
         description: "Back to the Future!",
         imgsrc: "img/car5.png",
-        rate: "4"
+        rate: 4
     },
     {
         id: 6,
@@ -45,7 +45,7 @@ var products = [{
         price: 500,
         description: "Battle Car made by our galactic neightbors to join the games. Now sold to Earth!",
         imgsrc: "img/car6.png",
-        rate: "2"
+        rate: 2
     },
     {
         id: 7,
@@ -53,7 +53,7 @@ var products = [{
         price: 999,
         description: "Formula One racer enters the battle!",
         imgsrc: "img/car7.png",
-        rate: "3"
+        rate: 3
     },
     {
         id: 8,
@@ -61,7 +61,7 @@ var products = [{
         price: 599,
         description: "Re-invention of a classic Earth-era Rocket battle car",
         imgsrc: "img/car8.png",
-        rate: "1.5"
+        rate: 1.5
     },
     {
         id: 9,
@@ -69,7 +69,7 @@ var products = [{
         price: 1250,
         description: "Designed by the Covenant to enter the games. Available for the first time!",
         imgsrc: "img/car9.png",
-        rate: "0.5"
+        rate: 0.5
     },
     {
         id: 10,
@@ -77,6 +77,6 @@ var products = [{
         price: 750,
         description: "HotWheels classic enters the battle!",
         imgsrc: "img/car10.png",
-        rate: "1"
+        rate: 1
     }
 ];
