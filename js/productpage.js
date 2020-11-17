032f078e02cbea49a53ded492af82a125795e971
UsaamahG <71952590+UG@users.noreply.github.com>
2020-11-16 23:07:11 -0500

    added $ to product page. Added Akshay's updated js code to productpage.

--- js/productpage.js
+++ js/productpage.js
@@ -1,50 +1,44 @@
+var currentIndex = -1;
+
 function loadProduct(index) {
     // loads products with index chosen to p
     var p = products[index];
     // grabs the title from the title class html
-    var title = document.getElementsByClassName("product-title").item(2);
-    var price = document.getElementsByClassName("priceinfo").item(index);
-    var description = document.getElementsByClassName("product-description").item(index);
-    var rating = document.getElementsByClassName("rate").item(index);
-    var description = document.getElementsByClassName("product-description").item(index);
+
+    var title =  document.getElementById("title1");
+    // gives access to the div that contains the price
+    var price = document.getElementById("priceDisplay");
+    // desc
+    var description = document.getElementsByClassName("product-description").item(0);
+
+    var rating = document.getElementsByClassName("rate").item(0);
+    var img = document.getElementsByClassName("product-image").item(0);
+    // img
+    var img = document.getElementsByClassName("product-image").item(0);
+    img.setAttribute("src" , p.imgsrc);
     title.innerText = p.title;
     price.innerText = p.price;
     description.innerText = p.description;
     rating.innerText = p.rate;
+
+    currentIndex = index;
 }
 
-loadProduct(2)
-
-
-function loadProduct(index) {
-    // loads products with index chosen to p
-    var p = products[index]
-        // grabs the title from the title class html
-    var title = document.getElementsByClassName("product-title").item(0)
-    var price = document.getElementsByClassName("priceinfo").item(0)
-    title.innerText = p.title
-    price.innerText = p.price
-    var description = document.getElementsByClassName("product-description").item(0)
-    description.innerText = p.description
-
-
-
-
-    var img = document.getElementsByClassName("product-image").item(0)
-    img.setAttribute("src", p.imgsrc)
-
-    updatePrice(p)
-
+window.onload = function() {
+    // product_id = the id in the URL
+    var product_id = window.location.search.split("id=")[1];
+    // loads products
+    loadProduct(product_id);
 }
 
-function updatePrice(p) {
-    var quantity = document.getElementsByName("quantityinput")
-    var price = document.getElementsByClassName("priceinfo").item(0)
-    var totalPrice = quantity * p.price
+function updatePrice() {
+    // gets the value of quantity
+    var quantity = document.getElementById("quantityInput").value;
 
-    price.innerText = p.price
+    var price_element = document.getElementById("priceDisplay");
+    // multiply quantity with product at current indexes price
+    var totalPrice = quantity * products[currentIndex].price;
 
-
-    console.log(totalPrice)
-
+    // price_element shows the new price;
+    price_element.innerText = totalPrice;
 }
